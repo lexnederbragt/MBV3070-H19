0c570351ab2768ff01c15c50cbb2c3a2287dfc2e
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2019-04-24 11:49:37 +0200

    Added comment

--- fix_doconce_pandoc.sh
+++ fix_doconce_pandoc.sh
@@ -1,3 +1,4 @@
+# script to fix formatting details between conversions
 sed 's/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/~~~\
 {: .language-python}/' $1 |
 sed 's/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.Python}/~~~/'
